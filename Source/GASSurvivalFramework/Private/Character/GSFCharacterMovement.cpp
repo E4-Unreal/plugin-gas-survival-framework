def3e3c25ba0d61f52ad76e6a3315eb644f07ff3
이정훈 <eu4ng97@gmail.com>
2024-03-07 16:39:28 +0900

    feat: Loose 게임플레이 태그 탈부착 기능이 포함된 GSFCharacterMovement 컴포넌트 클래스 작성
    점프, 앉기 등 기본 캐릭터 움직임 상태에 따라 자동으로 게임플레이 태그 부착 기능이 포함된 캐릭터 움직임 컴포넌트 클래스입니다.

---
+++ Source/GASSurvivalFramework/Private/Character/GSFCharacterMovement.cpp
@@ -0,0 +1,59 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/GSFCharacterMovement.h"
+
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemGlobals.h"
+#include "GSFGameplayTags.h"
+
+
+void UGSFCharacterMovement::InitializeComponent()
+{
+    Super::InitializeComponent();
+
+    // AbilitySystemComponent 약한 참조
+    OwnerAbilitySystem = UAbilitySystemGlobals::GetAbilitySystemComponentFromActor(GetOwner());
+}
+
+void UGSFCharacterMovement::SetMovementMode(EMovementMode NewMovementMode, uint8 NewCustomMode)
+{
+    // 게임플레이 태그 부착 혹은 제거
+    if(OwnerAbilitySystem.IsValid())
+    {
+        switch (NewMovementMode)
+        {
+        case MOVE_Falling:
+            OwnerAbilitySystem->AddLooseGameplayTag(GSFGameplayTags::State::FallingTag);
+            break;
+        default:
+            if(MovementMode == MOVE_Falling)
+            {
+                OwnerAbilitySystem->RemoveLooseGameplayTag(GSFGameplayTags::State::FallingTag);
+            }
+            break;
+        }
+    }
+
+    Super::SetMovementMode(NewMovementMode, NewCustomMode);
+}
+
+void UGSFCharacterMovement::Crouch(bool bClientSimulation)
+{
+    if(OwnerAbilitySystem.IsValid())
+    {
+        OwnerAbilitySystem->AddLooseGameplayTag(GSFGameplayTags::State::CrouchingTag);
+    }
+
+    Super::Crouch(bClientSimulation);
+}
+
+void UGSFCharacterMovement::UnCrouch(bool bClientSimulation)
+{
+    if(OwnerAbilitySystem.IsValid())
+    {
+        OwnerAbilitySystem->RemoveLooseGameplayTag(GSFGameplayTags::State::CrouchingTag);
+    }
+
+    Super::UnCrouch(bClientSimulation);
+}
