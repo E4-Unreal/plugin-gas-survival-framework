23329dd3bca4c9153b9bce4c3d832bcafa6abd10
이정훈 <eu4ng97@gmail.com>
2024-03-07 16:06:23 +0900

    feat: C++ 게임플레이 태그 선언 전용 파일 생성 및 기본 태그 등록

---
+++ Source/GASSurvivalFramework/Private/GSFGameplayTags.cpp
@@ -0,0 +1,24 @@
+﻿#include "GSFGameplayTags.h"
+
+namespace GSFGameplayTags
+{
+    namespace State
+    {
+        /* 기본 */
+        UE_DEFINE_GAMEPLAY_TAG(ProningTag, "State.Proning") // 엎드리기
+        UE_DEFINE_GAMEPLAY_TAG(CrouchingTag, "State.Crouching") // 앉기
+        UE_DEFINE_GAMEPLAY_TAG(FallingTag, "State.Falling") // 점프 혹은 낙하
+
+        /* 행동 불가 */
+        UE_DEFINE_GAMEPLAY_TAG(DeadTag, "State.Dead") // 죽음
+        UE_DEFINE_GAMEPLAY_TAG(StunTag, "State.Stun") // 무력화
+        UE_DEFINE_GAMEPLAY_TAG(KnockdownTag, "State.Knockdown") // 기절 (부활 기회)
+
+        /* 파쿠르 */
+        UE_DEFINE_GAMEPLAY_TAG(VaultingTag, "State.Vaulting") // 낮은 파쿠르
+        UE_DEFINE_GAMEPLAY_TAG(ClimbingTag, "State.Climbing") // 높은 파쿠르 혹은 사다리 타기
+
+        /* 슈터 */
+        UE_DEFINE_GAMEPLAY_TAG(ADSTag, "State.ADS") // 조준
+    }
+}
---
+++ Source/GASSurvivalFramework/Public/GSFGameplayTags.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace GSFGameplayTags
+{
+    namespace State
+    {
+        /* 기본 */
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(ProningTag)
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(CrouchingTag)
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(FallingTag)
+
+        /* 행동 불가 */
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(DeadTag)
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(StunTag)
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(KnockdownTag)
+
+        /* 파쿠르 */
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(VaultingTag)
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(ClimbingTag)
+
+        /* 슈터 */
+        UE_DECLARE_GAMEPLAY_TAG_EXTERN(ADSTag)
+    }
+}
