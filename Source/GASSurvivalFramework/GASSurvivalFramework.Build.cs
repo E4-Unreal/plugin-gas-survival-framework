5017f121fbebcf0983383cb099482b7fcd021d65
이정훈 <eu4ng97@gmail.com>
2024-03-07 15:29:27 +0900

    chore: 플러그인 및 모듈 종속성 추가

--- Source/GASSurvivalFramework/GASSurvivalFramework.Build.cs
+++ Source/GASSurvivalFramework/GASSurvivalFramework.Build.cs
@@ -7,46 +7,62 @@ public class GASSurvivalFramework : ModuleRules
 	public GASSurvivalFramework(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
 		PublicIncludePaths.AddRange(
-			new string[] {
-				// ... add public include paths required here ...
+			new string[]
+            {
+
 			}
 			);
-				
-		
+
+
 		PrivateIncludePaths.AddRange(
-			new string[] {
-				// ... add other private include paths required here ...
+			new string[]
+            {
+
 			}
 			);
-			
-		
+
+
 		PublicDependencyModuleNames.AddRange(
 			new string[]
 			{
+                // 기본
 				"Core",
-				// ... add other public dependencies that you statically link with here ...
+
+                // 이펙트
+                "Niagara",
+
+                // GAS
+                "GameplayAbilities",
+                "GameplayTags",
+                "GameplayTasks",
+
+                // 커스텀 플러그인
+                "GASExtension"
 			}
 			);
-			
-		
+
+
 		PrivateDependencyModuleNames.AddRange(
 			new string[]
 			{
+                // 기본
 				"CoreUObject",
 				"Engine",
 				"Slate",
 				"SlateCore",
-				// ... add private dependencies that you statically link with here ...	
+
+                // 애니메이션
+                "AnimGraphRuntime"
 			}
 			);
-		
-		
+
+
 		DynamicallyLoadedModuleNames.AddRange(
 			new string[]
 			{
-				// ... add any modules that your module loads dynamically here ...
+
 			}
 			);
 	}
