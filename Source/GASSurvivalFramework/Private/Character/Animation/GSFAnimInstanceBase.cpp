4993c93fe4ecdd4d30cc683c5886acec014d7436
이정훈 <eu4ng97@gmail.com>
2024-03-12 23:06:52 +0900

    feat: GEAnimInstance 클래스를 상속받은 GSFAnimInstance 클래스 작성
    기본적인 움직임 변수 설정 및 게임플레이 태그와 변수 바인딩

---
+++ Source/GASSurvivalFramework/Private/Character/Animation/GSFAnimInstanceBase.cpp
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Animation/GSFAnimInstanceBase.h"
+
+#include "GSFGameplayTags.h"
+#include "KismetAnimationLibrary.h"
+#include "GameFramework/Character.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+UGSFAnimInstanceBase::UGSFAnimInstanceBase()
+{
+    TagsToRegister.AddTagFast(GSFGameplayTags::State::FallingTag);
+    TagsToRegister.AddTagFast(GSFGameplayTags::State::CrouchingTag);
+    TagsToRegister.AddTagFast(GSFGameplayTags::State::ADSTag);
+    TagsToRegister.AddTagFast(GSFGameplayTags::State::DeadTag);
+    TagsToRegister.AddTagFast(GSFGameplayTags::State::CombatTag);
+}
+
+void UGSFAnimInstanceBase::NativeInitializeAnimation()
+{
+    Super::NativeInitializeAnimation();
+
+	Character = Cast<ACharacter>(GetOwningActor());
+	if(Character.IsValid())
+		CharacterMovement = Character->GetCharacterMovement();
+}
+
+void UGSFAnimInstanceBase::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
+{
+	// 유효성 검사
+	if(!Character.IsValid() || !CharacterMovement.IsValid()) return;
+
+	/* CharacterMovement */
+
+	// 속도
+	Velocity = CharacterMovement->GetLastUpdateVelocity();
+	GroundSpeed = FVector(Velocity.X, Velocity.Y, 0.f).Length();
+	bShouldMove = !CharacterMovement->GetCurrentAcceleration().IsNearlyZero() && GroundSpeed > 3.f;
+	Direction = UKismetAnimationLibrary::CalculateDirection(Velocity, Character->GetActorRotation());
+
+	// 에임 오프셋
+	const FRotator TargetRotator = (Character->GetControlRotation() - Character->GetActorRotation()).GetNormalized();
+	const FRotator CurrentRotator = FRotator(AimPitch, AimYaw, 0.f);
+	const FRotator DesiredRotator = FMath::RInterpTo(CurrentRotator, TargetRotator, DeltaSeconds, InterpSpeed);
+
+	AimPitch = FMath::Clamp(DesiredRotator.Pitch, -90.f, 90.f);
+	AimYaw = FMath::Clamp(DesiredRotator.Yaw, -90.f, 90.f);
+	AimYaw += AimYaw - DesiredRotator.Yaw; // 초과한 각도만큼 다시 원복함으로써 -180 에서 180 으로 한 번에 넘어가는 것을 방지
+}
+
+void UGSFAnimInstanceBase::OnGameplayEffectTagCountChanged_Event(const FGameplayTag Tag, int32 NewCount)
+{
+    Super::OnGameplayEffectTagCountChanged_Event(Tag, NewCount);
+
+    const bool bTriggered = NewCount > 0;
+
+    if(Tag.MatchesTag(GSFGameplayTags::State::FallingTag))
+        bFalling = bTriggered;
+    else if(Tag.MatchesTag(GSFGameplayTags::State::CrouchingTag))
+        bCrouching = bTriggered;
+    else if(Tag.MatchesTag(GSFGameplayTags::State::ADSTag))
+        bADS = bTriggered;
+    else if(Tag.MatchesTag(GSFGameplayTags::State::DeadTag))
+        bDead = bTriggered;
+    else if(Tag.MatchesTag(GSFGameplayTags::State::CombatTag))
+        bCombat = bTriggered;
+}
---
+++ Source/GASSurvivalFramework/Public/Character/Animation/GSFAnimInstanceBase.h
@@ -0,0 +1,79 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/GEAnimInstance.h"
+#include "GSFAnimInstanceBase.generated.h"
+
+class UCharacterMovementComponent;
+
+/**
+ * 속도, 방향 등 대부분의 캐릭터 애니메이션 블루프린트에서 필요한 값들을 멤버 변수로 지닌 클래스입니다.
+ */
+UCLASS()
+class GASSURVIVALFRAMEWORK_API UGSFAnimInstanceBase : public UGEAnimInstance
+{
+	GENERATED_BODY()
+
+	TWeakObjectPtr<ACharacter> Character;
+
+	TWeakObjectPtr<UCharacterMovementComponent> CharacterMovement;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|Movement", meta = (AllowPrivateAccess = true))
+	FVector Velocity;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|Movement", meta = (AllowPrivateAccess = true))
+	float GroundSpeed;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|Movement", meta = (AllowPrivateAccess = true))
+	float Direction;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|Movement", meta = (AllowPrivateAccess = true))
+	bool bShouldMove;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|Character", meta = (AllowPrivateAccess = true))
+	bool bFalling;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|Character", meta = (AllowPrivateAccess = true))
+	bool bCrouching;
+
+    UPROPERTY(BlueprintReadOnly, Category = "State|Character", meta = (AllowPrivateAccess = true))
+    bool bADS;
+
+    UPROPERTY(BlueprintReadOnly, Category = "State|Character", meta = (AllowPrivateAccess = true))
+    bool bCombat;
+
+    UPROPERTY(BlueprintReadOnly, Category = "State|Character", meta = (AllowPrivateAccess = true))
+    bool bDead;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|AimOffset", meta = (AllowPrivateAccess = true))
+	float AimYaw;
+
+	UPROPERTY(BlueprintReadOnly, Category = "State|AimOffset", meta = (AllowPrivateAccess = true))
+	float AimPitch;
+
+	UPROPERTY(BlueprintReadWrite, EditDefaultsOnly, Category = "State|AimOffset", meta = (AllowPrivateAccess = true))
+	float InterpSpeed = 15.f;
+
+public:
+    UGSFAnimInstanceBase();
+
+    /* AnimInstance */
+
+    virtual void NativeInitializeAnimation() override;
+    virtual void NativeThreadSafeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+    /* GEAnimInstance */
+
+    virtual void OnGameplayEffectTagCountChanged_Event(const FGameplayTag Tag, int32 NewCount) override;
+
+    /* Getter */
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE ACharacter* GetCharacter() const { return Character.Get(); }
+
+	UFUNCTION(BlueprintPure)
+	FORCEINLINE UCharacterMovementComponent* GetCharacterMovement() const { return CharacterMovement.Get(); }
+};
