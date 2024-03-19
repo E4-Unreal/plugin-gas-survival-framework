be464ce04971d2e61c97c31d11afeddaf645ccee
이정훈 <eu4ng97@gmail.com>
2024-03-20 03:59:48 +0900

    refactor: GASExtension 플러그인으로 이전

--- Source/GASSurvivalFramework/Private/GASSurvivalFramework.cpp
+++ Source/GASSurvivalFramework/Private/GASSurvivalFramework.cpp
@@ -6,24 +6,7 @@
 
 void FGASSurvivalFrameworkModule::StartupModule()
 {
-    /* 디테일 패널에 커스텀 섹션 추가 */
-#if WITH_EDITOR
-#define LOCTEXT_NAMESPACE "Custom Detail"
-    static const FName PropertyEditor("PropertyEditor");
-    FPropertyEditorModule& PropertyModule = FModuleManager::GetModuleChecked<FPropertyEditorModule>(PropertyEditor);
-
-    // Actor 클래스에 커스텀 섹션 생성 및 카테고리 추가
-    const TSharedRef<FPropertySection> ActorConfigSection = PropertyModule.FindOrCreateSection("Actor", "Config", LOCTEXT("Config", "설정"));
-    ActorConfigSection->AddCategory("Config");
-    ActorConfigSection->AddCategory("State");
-
-    // ActorComponent 클래스에 커스텀 섹션 생성 및 카테고리 추가
-    const TSharedRef<FPropertySection> ActorComponentConfigSection = PropertyModule.FindOrCreateSection("ActorComponent", "Config", LOCTEXT("Config", "설정"));
-    ActorComponentConfigSection->AddCategory("Config");
-    ActorComponentConfigSection->AddCategory("State");
-
-#undef LOCTEXT_NAMESPACE
-#endif
+    
 }
 
 void FGASSurvivalFrameworkModule::ShutdownModule()
